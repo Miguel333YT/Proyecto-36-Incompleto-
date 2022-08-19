7ab5d851591007a5c43fb27e65c751e53f42709d
Miguel333YT <97993047+Miguel333YT@users.noreply.github.com>
2022-08-18 21:09:49 -0500

    Add files via upload

---
+++ PROC36-1_4-plantilla-proyecto-main/sketch.js
@@ -0,0 +1,98 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
+
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloon = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+   /*balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon-02.png");
+   balloonImage3=loadAnimation("Images/HotAirBallon-03.png");*/
+   //,"Images/HotAirBallon-01.png",
+ 
+  // "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
+  // "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
+  }
+
+// Función para configurar el ambiente inicial
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,650,250,650);
+  balloon.addAnimation("hotAirBalloon",balloon);//age1,balloonImage2,balloonImage3);
+  balloon.scale=0.5;
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, console.log("error"));
+  textSize(20); 
+}
+
+// Función para mostrar UI
+function draw() {
+  background(bg);
+  background.addImage(bg);
+  
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloonImg.x=balloon.x-3;
+    // Agrega la animación del globo [usa balloonImage2]
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloonImg.x=balloon.x+3;
+    // Agrega la animación del globo [usa balloonImage2]
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloonImg.y=balloon.y+3;
+    // Agrega la animación del globo [usa balloonImage2]
+   // balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloonImg.y=balloon.y-3;
+    // Agrega la animación del globo [usa balloonImage2]
+    //balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**¡Usa las flechas del teclado para mover el globo aerostático!",40,40);
+
+}
+
+
+function updateHeight(x,y){
+  database.ref('/balloon/height').update({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
+
+
+
+
+function readHeight(data){
+
+  height = data.val();
+  ballon.x = height.x;
+  ballon.y = height.y;
+  
+  balloonHeight = data
+  var data = data.val();
+  this.positionX = data.positionX;
+  this.positionY = data.positionY;
+  
+
+  // Asigna el valor de "data" como la altura 
+  // Asigna el valor de "X" e "y" de la altura a las posiciones "x" e "y" respectivas del globo
+ }
+
+function showError(){
+  console.log("Error la escribir en la base de datos");
+}
+
+//maestra, no se porque no me aparece nada, si me lo pudiera decir se lo agradeceria mucho, gracias
